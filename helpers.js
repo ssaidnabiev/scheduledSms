ddd049e783f70fd93b21ef16d69a92989e337bf4
Saidazim <saidnabievsaidazim@gmail.com>
2026-01-17 15:01:29 +0500

    Fix

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const info = require('./info.js')
 
 function toSqlDateString(date) {
     const year = date.getFullYear()
@@ -21,23 +22,23 @@ function toSqlDateString(date) {
 async function sendErrorToGroup(err, origin='') {
     let error2 = null
     try {
-        const apiUrl = 'https://api.telegram.org/bot5849831361:AAFsY3mWq1S7cSDQxQgBBH9J3QG92PMDJVY/'
-        const groupID = -1003449990258
+        const apiUrl = `https://api.telegram.org/bot${info.tgBotToken}/`
+        const groupID = info.tgGroupChatId
         
         const resp = await fetch(`${apiUrl}sendMessage`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 chat_id: groupID,
-                text: 
+                text:
                     `<b>${String(os.hostname())}</b>\nCaught exception: ${err.message}\nException origin: ${origin}`,
                 parse_mode: 'HTML' 
             })
         })
     } catch (error) {
         try {
-            const apiUrl = 'https://api.telegram.org/bot8391939404:AAF6oVi6wdVsWzlLAKVIt1aZJ5P-9X4gz2s/'
-            const chatID = 7976312976
+            const apiUrl = `https://api.telegram.org/bot${info.tgBotToken}/`
+            const chatID = info.tgFallbackChatId
             
             const resp = await fetch(`${apiUrl}sendMessage`, {
                 method: 'POST',
